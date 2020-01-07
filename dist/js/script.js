233d6616ec18eb74b26533d7e3857a7c71f7a1e2
Luka <luka.kovacic@elatus.net>
2020-01-07 20:36:32 +0100

    test

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,6 +1,14 @@
-const handle = document.querySelector('.handle');
+const navSlide = () =>{
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.menu');
+    const social = document.querySelector('.social');
 
-handle.onclick= function(){
-    handle.classList.toggle('showing');
+    burger.addEventListener('click',()=>{
+        nav.classList.toggle('menu-active');
+        social.classList.toggle('social-active');
+        burger.classList.toggle('toggle');
+    })
+    
 }
 
+navSlide();
